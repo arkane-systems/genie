c2f0f75b438ded5eda801b83a5af35f9b9143161
Alistair Young <avatar@arkane-systems.net>
2019-06-18 00:05:12 -0500

    A little bit of .NET Core

---
+++ genie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace genie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
