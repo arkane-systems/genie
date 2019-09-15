3b162f4218d5d7e5840783934c2e2b75a56d181c
Alistair Young <avatar@arkane-systems.net>
2019-09-14 20:16:57 -0500

    Geniesh project.

---
+++ geniesh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace geniesh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
