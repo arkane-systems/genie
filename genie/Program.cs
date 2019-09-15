9f067429173bacc6e582ddb05e0abc010fb6c0ec
Alistair Young <avatar@arkane-systems.net>
2019-09-15 00:22:19 -0500

    Move hostname file to /run

--- genie/Program.cs
+++ genie/Program.cs
@@ -158,12 +158,12 @@ namespace ArkaneSystems.WindowsSubsystemForLinux.Genie
                    "initializing bottle failed; dumping WSL envars");
 
             // Generate new hostname.
-            Chain ("/bin/sh", "-c \"/bin/echo `hostname`-wsl > /etc/hostname-wsl\"",
+            Chain ("/bin/sh", "-c \"/bin/echo `hostname`-wsl > /run/hostname-wsl\"",
                    "initializing bottle failed; making new hostname");
 
             unsafe
             {
-                var bytes = Encoding.UTF8.GetBytes("/etc/hostname-wsl");
+                var bytes = Encoding.UTF8.GetBytes("/run/hostname-wsl");
                 fixed (byte* buffer = bytes)
                 {
                     chmod (buffer, Convert.ToUInt16 ("644", 8));
@@ -180,7 +180,7 @@ namespace ArkaneSystems.WindowsSubsystemForLinux.Genie
             }
 
             // Set the new hostname.
-            Chain ("/bin/mount", "--bind /etc/hostname-wsl /etc/hostname",
+            Chain ("/bin/mount", "--bind /run/hostname-wsl /etc/hostname",
                    "initializing bottle failed; bind mounting hostname");
 
             // Hosts file: check for new host name; if not there, update it.
@@ -371,7 +371,7 @@ namespace ArkaneSystems.WindowsSubsystemForLinux.Genie
             // At this point, we should be outside an existing bottle, one way or another.
 
             // It shouldn't matter whether we have setuid here, since we start the shell with
-            // login, which expects root and reassigns uid appropriately.
+            // runuser, which expects root and reassigns uid appropriately.
             StartShell(verbose);
 
             Unrootify();
