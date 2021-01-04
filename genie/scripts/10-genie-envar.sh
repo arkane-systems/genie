7604aa4b420c3f2223050c5dda00df06e1b42ffb
Alistair Young <avatar@arkane-systems.net>
2021-01-04 13:28:58 -0600

    Fixes #92 (additional).

--- genie/scripts/10-genie-envar.sh
+++ genie/scripts/10-genie-envar.sh
@@ -7,5 +7,5 @@ fi
 if [ -e /run/genie.path ]
 then
   PATH=$PATH:$(cat /run/genie.path)
-  echo PATH=$(echo $PATH | awk -v RS=: '!($0 in a) {a[$0]; printf("%s%s", length(a) > 1 ? ":" : "", $0)}')
+  echo "PATH=$(echo $PATH | awk -v RS=: '!($0 in a) {a[$0]; printf("%s%s", length(a) > 1 ? ":" : "", $0)}')"
 fi
