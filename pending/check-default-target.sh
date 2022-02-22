182ced5032984dbfc07fb39021422d0aafae74a6
Alistair Young <avatar@arkane-systems.net>
2022-02-22 14:04:08 -0600

    Remove old files.

--- pending/check-default-target.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-if [ $(basename $(readlink -f /etc/systemd/system/default.target)) != "multi-user.target" ];
-then
-  echo WARNING: the systemd default target is not set to multi-user.target;
-  echo          this is not supported and may cause issues.
-  echo          If you are sure you wish to use another target,
-  echo          this warning can be disabled in the config file.
-fi
