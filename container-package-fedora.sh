bf09d8a31ef8d4d2a8dfe3a42a567f634a0cac90
Alistair Young <avatar@arkane-systems.net>
2022-03-22 09:01:08 -0500

    Altpackage updates.

--- container-package-fedora.sh
+++ container-package-fedora.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 # This works on my machine with my local packaging container. It will not work for you.
+lxc exec package-arch --user 0 -- resolvectl dns eth0 172.16.0.128
 lxc exec package-fedora --user 1000 --cwd /pkg --env HOME=/home/avatar -- make package-fedora
